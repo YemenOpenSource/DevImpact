fe1aeeadea2e19b65cc920d42ae56ca74fee1d98
Osama Mabkhot <99215291+O2sa@users.noreply.github.com>
2026-04-10 06:33:16 +0300

    fix: Cannot find module '../components/language-provider' or its corresponding type declarations

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { LanguageProvider } from "../components/language-provider";
-import { ThemeProvider } from "../components/theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
